367156ee9ee6963835ed0a53ae681361b1e098c1
Sean McKibben <graphex@graphex.com>
2012-09-09 23:02:03 -0600

    new voices

--- app/models/say_actor.rb
+++ app/models/say_actor.rb
@@ -1,7 +1,8 @@
 require 'celluloid'
 class SayActor
   include Celluloid
-  VOICES = %w(Agnes Albert Alex Bad\ News Bahh Bells Boing Bruce Bubbles Cellos Deranged Fred Good\ News Hysterical Junior Kathy Pipe\ Organ Princess Ralph Trinoids Vicki Victoria Whisper Zarvox)
+  #VOICES = %w(Agnes Albert Alex Bad\ News Bahh Bells Boing Bruce Bubbles Cellos Deranged Fred Good\ News Hysterical Junior Kathy Pipe\ Organ Princess Ralph Trinoids Vicki Victoria Whisper Zarvox)
+  VOICES = %w(Daniel Emily Fiona Jill Karen Lee Moira Samantha Sangeeta Serena Tessa Tom)
 
   def initialize
     @voice = VOICES.shuffle[0]
